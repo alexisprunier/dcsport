914a67cb8d42fcf9bcb76005a4a43ddd7640de4f
alexisprunier <prunier.al@gmail.com>
2020-08-21 21:57:51 +0200

    Contract instanciation on webapp + progress on contract tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import "./App.css";
 import { ethers } from 'ethers';
+const DCSportArtifact = require('./../build/contracts/DCSport.json');
 
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            dcsport: null,
+        }
     }
 
     componentDidMount() {
         window.ethereum.enable()
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
+
+        var address = DCSportArtifact["networks"][provider.getNetwork]["address"];
+        var abi = DCSportArtifact["abi"];
+
+        this.setState({dcsport: new ethers.Contract(address, abi, signer)});
     }
 
    
     render() {
         return (
             <div className="App">
-                eee
+                {this.state.dcsport.bookmaker}
             </div>
         );
     }
