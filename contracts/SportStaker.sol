db398e9ca67e90ad4d94b5542e5aedd710813951
alexisprunier <prunier.al@gmail.com>
2020-08-27 18:12:57 +0200

    Introducing the ERC20 transfert and the SPORT stacking

--- contracts/SportStaker.sol
+++ contracts/SportStaker.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.5.9; 
 
-import "./SPORT.sol";
+import "./Sport.sol";
+import "./IERC20.sol";
 
-contract SPORTStaker {
+contract SportStaker {
+
+    // Bookmaker of the organisation
+    address public bookmaker;
 
     // In-app credit of bettors
     mapping(address => Stake) public stakes;
@@ -16,16 +20,17 @@ contract SPORTStaker {
         bool amount;
     }
 
-    constructor() public {
-        //sportToken = Sport;
+    constructor(address _sportToken) public {
+        bookmaker = msg.sender;
+        sportToken = IERC20(_sportToken);
     }
 
     function stack(uint _amount) public {
         require(sportToken.balanceOf(msg.sender) >= _amount);
 
-        /*if (stakes[msg.sender].amount) {
-            Stake memory s = Match(
-                block.timestamp + (60 * 60 * 24 * 30),
+        if (stakes[msg.sender].amount) {
+            Stake memory s = Stake(
+                block.timestamp + (60 * 60 * 24 * 30), // We lock the stacking for a month
                 _amount
             );
 
@@ -33,15 +38,18 @@ contract SPORTStaker {
         } else {
             stakes[msg.sender].amount += _amount;
             stakes[msg.sender].unlockTime = block.timestamp + (60 * 60 * 24 * 30);
-        }*/
+        }
+
+        require(sportToken.transferFrom(msg.sender, bookmaker, _amount), "Token trasfert failed"); 
     }
 
     function withdraw(uint _amount) public {
         require(sportToken.balanceOf(msg.sender) >= _amount, "The requested amount is bigger than the staked amount");
         require(stakes[msg.sender].unlockTime != 0, "You don't have any stake");
         require(stakes[msg.sender].unlockTime >= block.timestamp, "Your tokens are still blocked");
-        
 
+        stakes[msg.sender].amount -= _amount;
 
+        require(sportToken.transferFrom(bookmaker, msg.sender, _amount), "Token trasfert failed"); 
     }
 }
